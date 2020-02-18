2d48c57e291dea9b93ae3be584c4aef5b7a1a78f
Ruben <gallardogruben@gmail.com>
2020-02-18 10:07:39 +0100

    Finalizado cambio de fecha nacimiento y formato mobile finalizado

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-/*
-https://randomuser.me/api/?results=8
-
-mobile - username
-email
-birthday
-address
- */
-
 const info = document.querySelector('body');
 
 function fetchData(url, callback) {
@@ -22,17 +13,19 @@ function fetchData(url, callback) {
 
 function showProfile(response) {
     for (let i = 0; i < response.results.length; i++) {
-        console.log(response.results[i].date)
+        let day = response.results[i].dob.date.substr(8, 2);
+        let month = response.results[i].dob.date.substr(5, 2);
+        let year = response.results[i].dob.date.substr(0, 4);
         info.innerHTML += `
         <div class="container">
             <div class="row boxInfo">
-                <div class="col-sm-6 col-12 d-flex flex-column">
+                <div class="col-xl-6 col-12 d-flex flex-column">
                     <img src="${response.results[i].picture.large}" alt="">
                     <span class="name">${response.results[i].name.title}  ${response.results[i].name.first}  ${response.results[i].name.last}</span>
                     <span><i class="fas fa-phone-alt"></i> ${response.results[i].phone}</span>
                     <span><i class="far fa-user"></i> ${response.results[i].login.username}</span>
                     <span><i class="far fa-envelope"></i> ${response.results[i].email}</span>
-                    <span><i class="fas fa-birthday-cake"></i> ${response.results[i].date}</span>
+                    <span><i class="fas fa-birthday-cake"></i> ${day}-${month}-${year}</span>
                     <span><i class="fas fa-map"></i> ${response.results[i].location.street.number}, ${response.results[i].location.street.name}, ${response.results[i].location.city}, ${response.results[i].location.state}</span>
                 </div>
                 <div class="col-sm-6 col-12">
